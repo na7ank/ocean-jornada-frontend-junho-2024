b3a51fc8b6bc8ad524b196a6f1d5e0f2daff77b5
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-06-07 14:50:43 +0000

    Método map para arrays.

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,37 @@ function App() {
     image: 'https://doc.arcgis.com/en/insights/latest/create/GUID-5C7AAF44-C609-472D-9193-0E9B23C6B68F-web.png',
     status: 'Active',
     specie: 'BoxPlot',
-    origin: 'Regions'
+    origin: 'Regions',
   }
   const item2 = {
     title: 'Line Graph',
     image: 'https://raw.githubusercontent.com/na7ank/LinearRegression/main/imgs/output.png',
     status: 'Active',
     specie: 'Lines',
-    origin: 'Regression'
+    origin: 'Regression',
   }
   const item3 = {
     title: 'Scatter Graph',
     image: 'https://docs.tibco.com/pub/spotfire/6.5.2/doc/html/images/scat_example1.png',
     status: 'Active',
     specie: 'Scatter',
-    origin: 'Points'
+    origin: 'Points',
   }
   const item4 = {
     title: 'Normal Curve Graph',
     image: 'https://i0.wp.com/statisticsbyjim.com/wp-content/uploads/2018/09/normal_100.png?w=576&ssl=1',
     status: 'Nice',
     specie: 'Statistic',
-    origin: 'Curve'
+    origin: 'Curve',
   }
   // Listas Arrays
-  const itens = [item1, item2, item3, item4]
+  const itens = [item1, item2, item3, item4];
   return (
     <>
     <div className="cards">
       {/* Para cada um dos itens da lista, exibir um card*/}
       {itens.map(function (elemento) {
-        return <Card item={elemento}/>
+        return <Card item={elemento} key={elemento.title}/>
       })}
       <Card item = {item1} />
     </div>
