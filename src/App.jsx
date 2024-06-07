9a7825a9369dd4b40a025a64c56b5c2412f44196
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-06-07 14:17:04 +0000

    Enviando props para o component card.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-
+  const item1 = {
+    title: 'BloxPlot Graph',
+    image: 'https://doc.arcgis.com/en/insights/latest/create/GUID-5C7AAF44-C609-472D-9193-0E9B23C6B68F-web.png',
+    status: 'Active',
+    specie: 'BoxPlot',
+    origin: 'Regions'
+  }
+  const item2 = {
+    title: 'Line Graph',
+    image: 'https://raw.githubusercontent.com/na7ank/LinearRegression/main/imgs/output.png',
+    status: 'Active',
+    specie: 'Lines',
+    origin: 'Regression'
+  }
+  const item3 = {
+    title: 'Scatter Graph',
+    image: 'https://docs.tibco.com/pub/spotfire/6.5.2/doc/html/images/scat_example1.png',
+    status: 'Active',
+    specie: 'Scatter',
+    origin: 'Points'
+  }
   return (
     <>
     <div className="cards">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card item = {item1} />
+      <Card item = {item2} />
+      <Card item = {item3} />
     </div>
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,13 @@
-export default function Card(){
+export default function Card(props){
+    const item = props.item;
     return (
         <div className="card">
-            <h2>Title</h2>
-            <img src="https://doc.arcgis.com/en/insights/latest/create/GUID-5C7AAF44-C609-472D-9193-0E9B23C6B68F-web.png"/>
+            <h2>{item.title}</h2>
+            <img src={item.image}/>
             <div className="tags">
-                <div>Status: Active</div>
-                <div>Specie: BoxPlot</div>
-                <div>Origin: Regions</div>
+                <div>Status: {item.status}</div>
+                <div>Specie: {item.specie}</div>
+                <div>Origin: {item.origin}</div>
             </div>
         </div>
     )
