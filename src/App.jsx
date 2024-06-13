9916b2816b2880ca92537bd92590d9bd3eb828ae
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-06-13 12:34:10 +0000

    Fetch api início.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import './App.css'
 import Card from './components/Card/Card'
 
+
+
 function App() {
   
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
 
-  // Listas (Arrays)
-  const itens = [item1, item2, item3]
+  // // Listas (Arrays)
+  // const itens = [item1, item2, item3]
 
+  const itens = [];
+  const apiUrl = 'https://rickandmortyapi.com/api/character';
+  const response = fetch(apiUrl);
+  console.log(response);
+  
   return (
     <>
       <div className="cards">
-        {/* Para cada um dos itens da lista, exibir um Card */}
+        {/*
+        Para cada um dos itens da lista, exibir um Card 
+
+        -'item' contem os dados.
+        -'key' é necessária para diferenciar o elemento.
+        */}
         {itens.map(elemento => <Card item={elemento} key={elemento.name} />)}
       </div>
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,4 +13,4 @@ export default function Card(props) {
         </div>
       </div>
     )
-  }
+}
